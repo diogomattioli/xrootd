dafc8dfe5ff6d4ebf0c6b0e205fcef43af179572
Diogo Mattioli <diogo.mattioli@cern.ch>
2026-05-20 15:22:29 +0000

    chore: http third party copy job

--- src/XrdClHttp/XrdClHttpFilesystem.cc
+++ src/XrdClHttp/XrdClHttpFilesystem.cc
@@ -22,8 +22,70 @@
 #include "XrdClHttpFilesystem.hh"
 #include "XrdClHttpOps.hh"
 #include "XrdClHttpResponses.hh"
+#include "XrdCl/XrdClDefaultEnv.hh"
+#include "XrdCl/XrdClUtils.hh"
+
+#include <thread>
 
 using namespace XrdClHttp;
+
+ThirdPartyCopy::ThirdPartyCopy(uint32_t      jobId,
+                                XrdCl::PropertyList *jobProperties,
+                                XrdCl::PropertyList *jobResults,
+                                std::shared_ptr<XrdClHttp::HandlerQueue> queue,
+                                CreateConnCalloutType callout) :
+    CopyJob(jobId, jobProperties, jobResults),
+    m_queue(queue),
+    callout(callout)
+{
+    XrdCl::Log *log = XrdCl::DefaultEnv::GetLog();
+    log->Debug( XrdCl::UtilityMsg, "Creating a HTTP third party copy job, from %s to %s",
+                GetSource().GetObfuscatedURL().c_str(), GetTarget().GetObfuscatedURL().c_str() );
+}
+
+XrdCl::XRootDStatus ThirdPartyCopy::Run(XrdCl::CopyProgressHandler *progress)
+{
+    XrdCl::Log *log = XrdCl::DefaultEnv::GetLog();
+
+    log->Debug(kLogXrdClHttp, "XrdClHttp::ThirdPartyCopy Copy Op src %s dst %s", GetSource().GetURL().c_str(), GetTarget().GetURL().c_str());
+
+    std::shared_ptr<CurlStatOp> op_stat(new CurlStatOp(nullptr, GetSource().GetURL(), {10,0}, log, true, callout, nullptr));
+
+    try {
+        m_queue->Produce(op_stat);
+    } catch (...) {
+        log->Warning(kLogXrdClHttp, "Failed to add copy op to queue");
+        return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errOSError);
+    }
+
+    while (!op_stat->IsDone())
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+
+    if (op_stat->HasFailed())
+        return {123, 456, 789};
+
+    std::size_t size = op_stat->GetStatInfo().first;
+
+    std::shared_ptr<CurlCopyOp> op(new CurlCopyOp(nullptr, GetSource().GetURL(), {}, GetTarget().GetURL(), {}, {10, 0}, nullptr, callout));
+    op->SetCallback([this, progress, size] (auto bytemark)
+        {
+            progress->JobProgress(this->pJobId, bytemark, size);
+        });
+
+    try {
+        m_queue->Produce(op);
+    } catch (...) {
+        log->Warning(kLogXrdClHttp, "Failed to add copy op to queue");
+        return XrdCl::XRootDStatus(XrdCl::stError, XrdCl::errOSError);
+    }
+
+    while (!op->IsDone())
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+
+    progress->JobProgress(pJobId, size, size);
+
+    return XrdCl::XRootDStatus();
+}
 
 Filesystem::Filesystem(const std::string &url, std::shared_ptr<HandlerQueue> queue, XrdCl::Log *log)
     : m_queue(queue),
@@ -43,7 +105,7 @@ Filesystem::~Filesystem() noexcept {}
 
 std::unique_ptr<XrdCl::CopyJob> Filesystem::ThirdPartyCopy(uint32_t jobId, XrdCl::PropertyList *jobProperties, XrdCl::PropertyList *jobResults)
 {
-    return nullptr;
+    return std::make_unique<XrdClHttp::ThirdPartyCopy>(jobId, jobProperties, jobResults, this->m_queue, this->GetConnCallout());
 }
 
 XrdCl::XRootDStatus
--- src/XrdClHttp/XrdClHttpFilesystem.hh
+++ src/XrdClHttp/XrdClHttpFilesystem.hh
@@ -43,6 +43,22 @@ class Log;
 
 namespace XrdClHttp {
 
+class ThirdPartyCopy : public XrdCl::CopyJob
+{
+private:
+    std::shared_ptr<XrdClHttp::HandlerQueue> m_queue;
+    CreateConnCalloutType callout;
+
+public:
+    ThirdPartyCopy(uint32_t      jobId,
+                    XrdCl::PropertyList *jobProperties,
+                    XrdCl::PropertyList *jobResults,
+                    std::shared_ptr<XrdClHttp::HandlerQueue> queue,
+                    CreateConnCalloutType callout);
+    virtual ~ThirdPartyCopy() = default;
+    virtual XrdCl::XRootDStatus Run(XrdCl::CopyProgressHandler *progress = 0) override;
+};
+
 class HandlerQueue;
 
 class Filesystem final : public XrdCl::FileSystemPlugIn {
